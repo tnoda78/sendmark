2727b453212a68959187333e9debf245ffacab6e
tnoda-koga <t_noda@kogasoftware.com>
2017-05-08 12:31:49 +0900

    Fix coverall setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "sendmark"
 require 'coveralls'
 Coveralls.wear!
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "sendmark"
